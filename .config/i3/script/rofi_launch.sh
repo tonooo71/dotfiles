17516bcc03a723964f8bec7011369ca7cb87f0a1
maruoka <mrtm.conv.tky@gmail.com>
2018-07-16 03:04:38 +0900

    changed rofi interface

--- .config/i3/script/rofi_launch.sh
+++ .config/i3/script/rofi_launch.sh
@@ -4,9 +4,10 @@
 alpha="cc"   # opacity (00〜FF)
 
 options=(
-  -modi            "combi,system:$HOME/.config/i3/script/rofi_system.sh,run,ssh"
-  -combi-modi      "window,drun"
-  -show            "combi"
+# -modi            "combi,system:$HOME/.config/i3/script/rofi_system.sh,run,ssh"
+  -modi            "drun,window,system:$HOME/.config/i3/script/rofi_system.sh"
+#  -combi-modi      "window,drun"
+  -show            "drun"
   -font            "Monospace Bold 12"
   -width           "60"
   -padding         "40"
