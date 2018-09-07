21c5ab0629fb259d7162b3b3a636bf74e4804d51
maruoka <mrtm.conv.tky@gmail.com>
2018-09-08 07:39:44 +0900

    Sat Sep  8 07:39:44 JST 2018

---
+++ .config/i3/script/notify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+notify-send `$HOME/.config/polybar/scripts/network.sh click`
+notify-send `$HOME/.config/polybar/scripts/memory.sh click`
+notify-send `$HOME/.config/polybar/scripts/battery.sh click`
+notify-send `$HOME/.config/polybar/scripts/volume.sh click`
