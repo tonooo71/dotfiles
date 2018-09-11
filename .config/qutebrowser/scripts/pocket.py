a7388497ebeaa9b54aff258b883c1041bf177ba8
maruoka <mrtm.conv.tky@gmail.com>
2018-09-11 10:16:59 +0900

    Tue Sep 11 10:16:59 JST 2018

--- .config/qutebrowser/scripts/pocket.py
+++ .config/qutebrowser/scripts/pocket.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import requests
 import sys
-
+import pocket_key
 
 url = sys.argv[1]
-consumer_key = '80458-d514f08089c5339086ab2369'
+consumer_key = pocket_key.consumer_key
 access_token = '125a4db7-f3f4-3896-aa5c-5dd5a5'
 
 requests.post('https://getpocket.com/v3/add', {'url': url, 'consumer_key': consumer_key, 'access_token': access_token})
