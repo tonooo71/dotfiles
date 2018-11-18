d87b725af3fefcf9cc656f4b9fc1d3b84653af3f
maruoka <mrtm.conv.tky@gmail.com>
2018-11-18 21:06:06 +0900

    Sun Nov 18 21:06:06 JST 2018

--- .config/polybar/scripts/add_ws.py
+++ .config/polybar/scripts/add_ws.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python
-import subprocess
-import i3
+import i3ipc
 
-max_ws = 0
-ws_list = [[x, 0] for x in range(11)]
-# get a list of workspaces
-for workspace in i3.get_workspaces():
-    # get the workspace tree data
-    workspace_tree = i3.filter(num=workspace.get('num'))
-    ws_list[int(workspace.get('num'))][1] = 1
-for i in ws_list:
-    if i[0] > max_ws and not i[1]:
-        subprocess.run(['i3-msg', 'workspace', str(i[0])])
+i3 = i3ipc.Connection()
+i = 1
+for w in i3.get_workspaces():
+    if i != w['num']:
+        i3.command(f'workspace {i}')
         break
+    i += 1
+else:
+    i3.command(f'workspace {i}')
